f26d74a2492ef3ae9a1b01e8ab327142fbb0eda3
tuna1025 <jo215098@gmail.com>
2024-10-02 00:58:55 +0900

    1주차 과제

---
+++ 이동원_BOJ_1780.py
@@ -0,0 +1,29 @@
+def check(row, col, num, paper):
+    start = paper[row][col]
+    for i in range(row, row + num):
+        for j in range(col, col + num):
+            if start != paper[i][j]:
+                return False
+    return True
+
+def divide(row, col, num, paper, result):
+    if check(row, col, num, paper):
+        result[paper[row][col]] += 1
+    else:
+        size = num // 3
+        for i in range(3):
+            for j in range(3):
+                divide(row + size * i, col + size * j, size, paper, result)
+
+N = int(input())
+paper = []
+for i in range(N):
+    row = list(map(int, input().split()))
+    row = [x + 1 for x in row]  # -1, 0, 1을 0, 1, 2로 변환
+    paper.append(row)
+
+result = [0, 0, 0]
+divide(0, 0, N, paper, result)
+print(result[0])
+print(result[1])
+print(result[2])
